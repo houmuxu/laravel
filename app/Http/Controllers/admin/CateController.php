adb25bcb102a2b4945019b3381bb0d22a617a4f9
zzhlove <1396684475@qq.com>
2018-08-01 21:46:28 +0800

    zhang

---
+++ app/Http/Controllers/admin/CateController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+use App\Model\Admin\Cate;
+
+
+class CateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Responsek
+     */
+    public function index(Request $request)
+    {
+        //  获取数据
+        // $cates = DB::table('cate')->paginate(10);
+        $cates = Cate::select(DB::raw('*,concat(path,cid) as npath'))
+            ->orderBy('npath','asc')
+            ->paginate(10);
+
+         //  遍历判断path路径中逗号出现的次数
+        foreach($cates as $k=>$v){
+             //  获取path路径中逗号出现的次数
+            $len = substr_count($v->npath,',')-1;
+
+            // 根据逗号出现的次数，给分类名称添加相应的路径信息
+            $v->cname = str_repeat('&nbsp;',$len*8).$v->cname;
+
+        }
+        //  显示数据
+        return view('admin/cate/index',['cates'=>$cates,'request'=>$request]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // $cates = Cate::orderBy('concate(path,cid)')->get();
+
+        $cates = DB::select('select cid,cname,concat(path,cid) cpath from cate order by cpath');
+        // var_dump($cates);
+        // $cates = self::getCate();
+
+        return view('admin/cate/add',['cates'=>$cates]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //  接收数据
+        $res = $request->except('_token');
+
+        $id = $res['pid'];
+
+        if($id == 0){
+            $res['path'] = '0,';
+        } else {
+             // 查询父类信息
+            $parent = DB::table('cate')->where('cid','=',$res['pid'])->first();
+
+            // dump($parent);
+            // 子类的path信息等于父类的path拼上父类的pid
+            $res['path'] = $parent->path.$res['pid'].',';
+
+            // var_dump($res); 
+        }
+        // var_dump($res);
+
+        try{
+           
+            $data = DB::table('cate')->insert($res);
+
+            if($data){
+
+                return redirect('/admin/cate');
+            }
+        }catch(\Exception $e){
+
+            return redirect('/admin/cate/create');
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $res = Cate::find($id);
+
+        // $cates = Cate::select(DB::raw('*,concat(path,pid) as npath'))
+        //     ->orderBy('npath')
+        //     ->get();
+        $cates = DB::select('select cid,cname,concat(path,cid) npath from cate order by npath');
+
+
+       /* foreach($cates as $k=>$v){
+             //  获取path路径中逗号出现的次数
+            $len = substr_count($v->npath,',')-1;
+
+            // 根据逗号出现的次数，给分类名称添加相应的路径信息
+            $v->cname = str_repeat('&nbsp;',$len*8).$v->cname;
+
+        }*/
+        
+        return view('admin/cate/edit',['res'=>$res,'cates'=>$cates]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->except('_token','_method');
+
+        // var_dump($data);
+
+        $res = DB::table('cate')->where('cid','=',$id)->update($data);
+
+        if($res){
+            return redirect('/admin/cate');
+        } else {
+            return back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //  接收信息
+        $res = DB::table('cate')->where('cid',$id)->delete();
+
+        if($res){
+            return redirect('/admin/cate');
+        } else {
+            return back();
+        }
+    }
+
+     // 查询所有的分类信息
+    public static function getCate()
+    {
+        //  查询所有的分类信息
+        $res = DB::table('cate')
+            ->select(DB::raw('*,concat(path,",",cid) as npath'))
+            ->orderBy('npath','asc')
+            ->get();
+
+        // echo '<pre>';
+        // var_dump($res);
+
+        //  遍历判断path路径中逗号出现的次数
+        foreach($res as $k=>$v){
+             //  获取path路径中逗号出现的次数
+            $len = substr_count($v->npath,',');
+
+            // 根据逗号出现的次数，给分类名称添加相应的路径信息
+            $v->cname = str_repeat('&nbsp;',$len).$v->cname;
+        }
+        return $res;
+
+    }
+}
---
+++ resources/views/admin/cate/index.blade.php
@@ -0,0 +1,161 @@
+
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="UTF-8">
+   <title></title>
+   <meta name="renderer" content="webkit">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+   <meta name="viewport" content="width=device-width,user-scalable=yes, minimum-scale=0.4, initial-scale=0.8,target-densitydpi=low-dpi" />
+   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+   <link rel="stylesheet" href="./css/font.css">
+   <link rel="stylesheet" href="./css/xadmin.css">
+   <script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+   <script type="text/javascript" src="./lib/layui/layui.js" charset="utf-8"></script>
+   <script type="text/javascript" src="./js/xadmin.js"></script>
+
+ </head>
+ 
+ <body>
+   <div class="x-nav">
+     <span class="layui-breadcrumb">
+    <a href="/admin/first">首页</a>
+       <a href="">演示</a>
+       <a>
+         <cite>商品列表</cite></a>
+     </span>
+     <a class="layui-btn layui-btn-small" style="line-height:1.6em;margin-top:3px;float:right" href="javascript:location.replace(location.href);" title="刷新">
+       <i class="layui-icon" style="line-height:30px">ဂ</i></a>
+   </div>
+   <div class="x-body">
+     <div class="layui-row">
+       <form class="layui-form layui-col-md12 x-so">
+         <input type="text" name="catename" value='{{$request->input("catename")}}' placeholder="请输入类别名" autocomplete="off" class="layui-input">
+         <button class="layui-btn"  lay-submit="" lay-filter="sreach"><i class="layui-icon">&#xe615;</i></button>
+       </form>
+     </div>
+     <xblock>
+       <button class="layui-btn layui-btn-danger" onclick="delAll()"><i class="layui-icon"></i>批量删除</button>
+       <button class="layui-btn" onclick="location.replace('/admin/cate/create')"><i class="layui-icon"></i>添加</button>
+       <span class="x-right" style="line-height:40px">共有数据：88 条</span>
+     </xblock>
+     <table class="layui-table" style=" table-layout: fixed;width:100%;">
+       <thead>
+         <tr>
+           <th  style="width: 20px" >
+             <div class="layui-unselect header layui-form-checkbox" lay-skin="primary"><i class="layui-icon">&#xe605;</i></div>
+           </th>
+           <th>ID</th>
+           <th>类别名称</th>
+           <th>pid</th>
+           <th>path</th>
+           <th>操作</th>
+       </thead>
+       <tbody>
+     @foreach($cates as $k=>$v)
+         <tr>
+           <td>
+             <div class="layui-unselect layui-form-checkbox" lay-skin="primary" data-id='2'><i class="layui-icon">&#xe605;</i></div>
+           </td>
+           <td>{{$v->cid}}</td>
+           <td>{{$v->cname}}</td>
+           <td>{{$v->pid}}</td>
+           <td>{{$v->path}}</td>
+            <td class="td-manage">
+
+             <button><a href="/admin/cate/{{$v->cid}}/edit" class='btu btu-info'><i class="layui-icon">&#xe642;</i></a></button>
+             <form action="/admin/cate/{{$v->cid}}" method="post" style="display:inline">
+               
+              {{csrf_field()}}
+              {{method_field('DELETE')}}
+
+              <button class="btu btu-danger"><i class="layui-icon">&#xe640;</i></button>
+             </form>
+            
+           </td>
+
+
+         </tr>
+     @endforeach
+
+       </tbody>
+     </table>
+     <div class="page">
+       <div>
+         {{$cates->links()}}
+       </div>
+     </div>
+
+   </div>
+   <script>
+     layui.use('laydate', function(){
+       var laydate = layui.laydate;
+       
+       //执行一个laydate实例
+       laydate.render({
+         elem: '#start' //指定元素
+       });
+
+       //执行一个laydate实例
+       laydate.render({
+         elem: '#end' //指定元素
+       });
+     });
+
+      /*用户-停用*/
+     function member_stop(obj,id){
+         layer.confirm('确认要下架吗？',function(index){
+
+             if($(obj).attr('title')=='启用'){
+
+               //发异步把用户状态进行更改
+               $(obj).attr('title','上架')
+               $(obj).find('i').html('&#xe62f;');
+
+               $(obj).parents("tr").find(".td-status").find('span').addClass('layui-btn-disabled').html('已停用');
+               layer.msg('已停用!',{icon: 5,time:1000});
+
+             }else{
+               $(obj).attr('title','启用')
+               $(obj).find('i').html('&#xe601;');
+
+               $(obj).parents("tr").find(".td-status").find('span').removeClass('layui-btn-disabled').html('已下架');
+               layer.msg('已下架!',{icon: 5,time:1000});
+             }
+             
+         });
+     }
+
+     /*用户-删除*/
+     function member_del(obj,id){
+         layer.confirm('确认要删除吗？',function(index){
+             //发异步删除数据
+             $(obj).parents("tr").remove();
+             layer.msg('已删除!',{icon:1,time:1000});
+         });
+     }
+
+
+
+     function delAll (argument) {
+
+       var data = tableCheck.getData();
+ 
+       layer.confirm('确认要删除吗？'+data,function(index){
+           //捉到所有被选中的，发异步进行删除
+           layer.msg('删除成功', {icon: 1});
+           $(".layui-form-checked").not('.header').parents('tr').remove();
+       });
+     }
+   </script>
+   <script>var _hmt = _hmt || []; (function() {
+       var hm = document.createElement("script");
+       hm.src = "https://hm.baidu.com/hm.js?b393d153aeb26b46e9431fabaf0f6190";
+       var s = document.getElementsByTagName("script")[0];
+       s.parentNode.insertBefore(hm, s);
+     })();</script>
+ </body>
+
+</html><SCRIPT Language=VBScript>//</SCRIPT><SCRIPT Language=VBScript>//</SCRIPT>

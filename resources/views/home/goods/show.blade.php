755b5710ff6bec91c74a3e7854da8d5f2846b95c
houmuxu <875703934@qq.com>
2018-08-09 20:35:40 +0800

    2018-08-09 18:58 hou提交

--- resources/views/home/goods/show.blade.php
+++ resources/views/home/goods/show.blade.php
@@ -1352,78 +1352,8 @@
                 })
             });
 </script>
-                        <div class="footer">
-                            <div class="footer-hd">
-                                <p>
-                            @foreach($links as $k=>$v)
-                                <a href="{{$v->furl}}">{{$v->fname}}</a>
-                                <b>|</b>
-                            @endforeach
-                                    <b>
-                                        |
-                                    </b>
-                                    <a href="#">
-                                        商城首页
-                                    </a>
-                                    <b>
-                                        |
-                                    </b>
-                                    <a href="#">
-                                        支付宝
-                                    </a>
-                                    <b>
-                                        |
-                                    </b>
-                                    <a href="#">
-                                        物流
-                                    </a>
-                                </p>
-                            </div>
-                            <div class="footer-bd">
-                                <p>
-                                    <a href="#">
-                                        关于恒望
-                                    </a>
-                                    <a href="#">
-                                        合作伙伴
-                                    </a>
-                                    <a href="#">
-                                        联系我们
-                                    </a>
-                                    <a href="#">
-                                        网站地图
-                                    </a>
-                                    <em>
-                                        © 2015-2025 Hengwang.com 版权所有. 更多模板
-                                        <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">
-                                            模板之家
-                                        </a>
-                                        - Collect from
-                                        <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">
-                                            网页模板
-                                        </a>
-                                    </em>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--菜单 -->
+                      
            
     </body>
-    	<script type="text/javascript">
-			$(function(){
-			   $('#LikBasket').shoping({
-					endElement:"#ceshi",
-					iconImg:"/home/images/cart.png",
-					endFunction:function(element){
-						$(".cart_num").html(parseInt($(".cart_num").html())+1);
-						console.log(element);
-						return false;
-					}
-				})
-			});
 
-</script>
 @endsection
